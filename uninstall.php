4b1fcaf390210f7a7b46cccdaf516033ff806093
Chick <56658124+IROpany@users.noreply.github.com>
2024-10-01 17:46:26 +0900

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -5,16 +5,16 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 // いいねカウントメタデータを削除する関数
-function custom_like_button_delete_plugin() {
+function shortcode_like_button_delete_plugin() {
     global $wpdb;
 
     // すべての投稿メタデータを削除
-    $wpdb->query( "DELETE FROM {$wpdb->postmeta} WHERE meta_key = 'custom_like_count'" );
+    $wpdb->query( "DELETE FROM {$wpdb->postmeta} WHERE meta_key = 'shortcode_like_count'" );
 
     // 必要に応じて、プラグインで作成されたその他のオプションやデータを削除
-    // delete_option('custom_like_button_option');
+    // delete_option('shortcode_like_button_option');
 }
 
 // アンインストール時にクリーンアップ関数を呼び出す
-custom_like_button_delete_plugin();
+shortcode_like_button_delete_plugin();
 ?>
